039eb94d1769a4dd5290355b9c2b81ff95218e8e
castillo420 <castillojulian708@gmail.com>
2026-06-25 17:43:24 -0300

    .

--- Backend/src/DB/mySQL.js
+++ Backend/src/DB/mySQL.js
@@ -6,6 +6,7 @@ const dbconfig = {
   user: config.mysql.user,
   password: config.mysql.password,
   database: config.mysql.database,
+  port: config.mysql.port,
 };
 
 let conexion;
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 //middleware
 app.use(
   cors({
-    origin: "https://mundo-azul-s5ug.vercel.app/",
+    origin: "https://mundo-azul-s5ug.vercel.app",
     credentials: true,
   })
 );
